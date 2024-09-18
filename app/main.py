777df459818c826a79e489759cacc801e1586c6f
Anastasiia Mylnikova <87014295+amylnikova@users.noreply.github.com>
2024-09-18 18:56:13 +0300

    Linter formatter task solution

--- app/main.py
+++ app/main.py
@@ -1,13 +1,27 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
+    return {
+        new_key: error[old_key] for old_key, new_key
+        in {
+            "line_number": "line",
+            "column_number": "column",
+            "text": "message",
+            "code": "name"
+        }.items()
+        if old_key in error
+    } | {"source": "flake8"}
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+    return {
+        "errors": [format_linter_error(error) for error in errors],
+        "path": file_path,
+        "status": "passed" if not errors else "failed"
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+    return [
+        format_single_linter_file(key, val)
+        for key, val
+        in linter_report.items()
+    ]
